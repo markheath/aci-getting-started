922901801f4ad1a13db5767c739a709cd9d64eed
Mark Heath <mark.heath@gmail.com>
2018-03-20 20:21:42 +0000

    getting the private container registry demo working

--- m2-04-private-container-registry.ps1
+++ m2-04-private-container-registry.ps1
@@ -1,25 +1,33 @@
+# create a resource group to use
 $resourceGroup = "AciPrivateRegistryDemo"
 $location = "westeurope"
 az group create -n $resourceGroup -l $location
 
-$acrName = "my-acr"
-az acr create -g $resourceGroup -n $acrName --sku Basic
-az acr login --name $acrName
-$loginServer = az acr show --name $acrName --query loginServer --output tsv
+# create an Azure Container Registry
+$acrName = "mheathacr"
+az acr create -g $resourceGroup -n $acrName --sku Basic --admin-enabled true
 
-$imageName = "my-image"
-$imageTag = "$loginServer/$($imageName):v1"
-docker tag $imageName $imageTag
-docker push $imageTag
-az acr repository list --name $acrName --output table
+# docker login to ACR (assumes we have docker installed) - decided against this as it gave scary warning about disabling wincred
+# az acr login --name $
+
+# az acr update -n $acrName --admin-enabled true
 
 $acrUser = (az acr show --name $acrName --query loginServer -o tsv)
 $acrPassword = (az acr credential show --name $acrName --query "passwords[0].value" -o tsv)
+$loginServer = az acr show --name $acrName --query loginServer --output tsv
+docker login -u $acrName -p $acrPassword $loginServer 
+
+$imageName = "mystaticsite"
+$imageTag = "$loginServer/$($imageName):v1"
+docker tag "$($imageName):v1" $imageTag
+docker push $imageTag
+az acr repository list --name $acrName --output table
+
 
 $containerGroupName = "aci-acr"
 az container create --resource-group $resourceGroup --name $containerGroupName --image $imageTag `
     --cpu 1 --memory 1 --registry-username $acrUser --registry-password $acrPassword `
-    --dns-name-label aci-acr --ports 80
+    --dns-name-label "aciacr" --ports 80
 
 
 $fqdn = az container show -g $resourceGroup -n $containerGroupName --query ipAddress.fqdn -o tsv
@@ -28,6 +36,6 @@ $site = "http://$($fqdn)"
 
 Start-Process $site
 
-az container logs -n $containerGroupName -g $resourceGroup 
+az container logs -n $containerGroupName -g $resourceGroup
 
 az group delete -n $resourceGroup -y

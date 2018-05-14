c67d5463e209e8d4b87d78508c0641013855e774
Mark Heath <mark.heath@gmail.com>
2018-05-14 20:49:05 +0100

    updates to ghost blog demo

--- m2-01-ghost-blog-cli.ps1
+++ m2-01-ghost-blog-cli.ps1
@@ -5,11 +5,23 @@ az group create -n $resourceGroup -l $location
 
 # create a new container group using the ghost blog image
 $containerGroupName = "ghost-blog1"
-az container create -g $resourceGroup -n $containerGroupName --image ghost `
-          –ports 2368 –ip-address public –dns-name-label ghostaci 
+az container create `
+    -g $resourceGroup -n $containerGroupName `
+    --image ghost `
+    --ports 2368 `
+    --ip-address public `
+    --dns-name-label ghostaci 
+
+# show container info
+az container show `
+    -g $resourceGroup -n $containerGroupName
 
 # find out the domain name
-$fqdn = az container show -g $resourceGroup -n $containerGroupName --query ipAddress.fqdn -o tsv
+$fqdn = az container show `
+    -g $resourceGroup `
+    -n $containerGroupName `
+    --query ipAddress.fqdn `
+    -o tsv
 
 # visit the blog home page
 $site = "http://$($fqdn):2368"
@@ -20,7 +32,8 @@ Start-Process $site
 Start-Process "$site/ghost"
 
 # check the logs for this container group
-az container logs -n $containerGroupName -g $resourceGroup 
+az container logs `
+    -n $containerGroupName -g $resourceGroup 
 
 # delete the resource group including the container group
 az group delete -n $resourceGroup -y

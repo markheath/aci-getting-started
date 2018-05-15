ba17ccaf752c283156ba05eb1679913b0619c44f
Mark Heath <mark.heath@gmail.com>
2018-05-15 22:23:27 +0100

    cake build demo now can be completely done with CLI

--- m3-02-mounting-git-repo.ps1
+++ m3-02-mounting-git-repo.ps1
@@ -6,17 +6,20 @@ az group create -n $resourceGroup -l $location
 
 # we want our ACI container to deploy to a web app, so let's create an app service plan...
 $planName = "AciDemo"
-az appservice plan create -n $planName -g $resourceGroup -l $location --sku B1
+az appservice plan create -n $planName -g $resourceGroup `
+    -l $location --sku B1
 
 # ... and a web app
 $appName = "acicake"
 az webapp create -n $appName -g $resourceGroup --plan $planName
 
 # get the deployment credentials
-$user = az webapp deployment list-publishing-profiles -n $appName -g $resourceGroup `
+$user = az webapp deployment list-publishing-profiles `
+    -n $appName -g $resourceGroup `
     --query "[?publishMethod=='MSDeploy'].userName" -o tsv
 
-$pass = az webapp deployment list-publishing-profiles -n $appName -g $resourceGroup `
+$pass = az webapp deployment list-publishing-profiles `
+    -n $appName -g $resourceGroup `
     --query "[?publishMethod=='MSDeploy'].userPWD" -o tsv
 
 function CreateContainerCli  {
@@ -24,16 +27,31 @@ function CreateContainerCli  {
     # this is with some made up command line arguments of --gitrepo-repository and 
     # --gitrepo-mount-path to show mounting a github repository
     # we are also setting environment variables
-    $containerGroupName = "transcode"
-    az container create `
-        -g $resourceGroup `
-        -n $containerGroupName `
-        --image markheath/cakebuilder `
-        --gitrepo-repository https://github.com/markheath/aspnet-core-cake `
-        --gitrepo-mount-path "/src" `
-        -e KUDU_CLIENT_BASEURI=https://$appName.scm.azurewebsites.net KUDU_CLIENT_USERNAME=$user KUDU_CLIENT_PASSWORD=$pass `
-        --restart-policy never `
-        --command-line "./build.sh -Target=Default --settings_skipverification=true"
+$containerGroupName = "cakebuilder"
+#$commandLine = "/bin/bash -c ""chmod 755 ./build.sh && ./build.sh -Target=Default --settings_skipverification=true"""
+az container create `
+    -g $resourceGroup `
+    -n $containerGroupName `
+    --image "markheath/cakebuilder:0.1" `
+    --gitrepo-url https://github.com/markheath/aspnet-core-cake `
+    --gitrepo-mount-path "/src" `
+    --restart-policy never `
+    -e KUDU_CLIENT_BASEURI=https://$appName.scm.azurewebsites.net `
+       KUDU_CLIENT_USERNAME=$user KUDU_CLIENT_PASSWORD=$pass `
+    --command-line "/bin/bash -c 'chmod 755 ./build.sh && ./build.sh -Target=Default --settings_skipverification=true'"
+}
+
+function ArmDeploy {
+    # instead we'll deploy using an ARM template
+$containerGroupName = "cakebuilder"
+az group deployment create `
+    -n TestDeployment -g $resourceGroup `
+    --template-file "cake-builder.json" `
+    --parameters "KUDU_CLIENT_BASEURI=https://$appName.scm.azurewebsites.net" `
+    --parameters "KUDU_CLIENT_USERNAME=$user" `
+    --parameters "KUDU_CLIENT_PASSWORD=$pass" `
+    --parameters "containerGroupName=$containerGroupName"
+#    --parameters commandLine='./build.sh -Target=Default --settings_skipverification=true'
 }
 
 function LocalDockerTest {
@@ -46,19 +64,11 @@ function LocalDockerTest {
 }
 
 
-# instead we'll deploy using an ARM template
-$containerGroupName = "cakebuilder"
-az group deployment create `
-    -n TestDeployment -g $resourceGroup `
-    --template-file "cake-builder.json" `
-    --parameters "KUDU_CLIENT_BASEURI=https://$appName.scm.azurewebsites.net" `
-    --parameters "KUDU_CLIENT_USERNAME=$user" `
-    --parameters "KUDU_CLIENT_PASSWORD=$pass" `
-    --parameters "containerGroupName=$containerGroupName"
-#    --parameters commandLine='./build.sh -Target=Default --settings_skipverification=true'
+
 
 # enable run from zip deployment technique (currently more reliable if we set it after our first upload to sitepackages)
-az webapp config appsettings set -n $appName -g $resourceGroup --settings WEBSITE_RUN_FROM_ZIP=1
+az webapp config appsettings set -n $appName -g $resourceGroup `
+    --settings WEBSITE_RUN_FROM_ZIP=1
 
 # see what's in Kudu
 Start-Process https://$appName.scm.azurewebsites.net

dc9bc23d576286654140b7e6545e5fda3d37809b
Mark Heath <mark.heath@gmail.com>
2018-05-15 22:23:07 +0100

    formatting file share volume mount demo

--- m3-01-mounting-azure-file-share.ps1
+++ m3-01-mounting-azure-file-share.ps1
@@ -3,14 +3,19 @@ $resourceGroup = "AciVolumeDemo"
 $location = "westeurope"
 az group create -n $resourceGroup -l $location
 
-$storageAccountName = "acishare$(Get-Random -Minimum 1000 -Maximum 10000)"
+$storageAccountName = "acishare$(Get-Random `
+    -Minimum 1000 -Maximum 10000)"
 
 # create a storage account
-az storage account create -g $resourceGroup -n $storageAccountName `
+az storage account create -g $resourceGroup `
+    -n $storageAccountName `
     --sku Standard_LRS
 
 # get the connection string for our storage account
-$storageConnectionString = az storage account show-connection-string -n $storageAccountName -g $resourceGroup --query connectionString -o tsv
+$storageConnectionString = `
+    az storage account show-connection-string `
+    -n $storageAccountName -g $resourceGroup `
+    --query connectionString -o tsv
 # export it as an environment variable
 $env:AZURE_STORAGE_CONNECTION_STRING = $storageConnectionString
 
@@ -24,9 +29,15 @@ $localFile = "C:\Users\markh\Pictures\Camera Roll\$filename"
 az storage file upload -s $shareName --source "$localFile"
 
 # get the key for this storage account
-$storageKey=$(az storage account keys list -g $resourceGroup --account-name $storageAccountName --query "[0].value" --output tsv)
+$storageKey=$(az storage account keys list `
+    -g $resourceGroup --account-name $storageAccountName `
+    --query "[0].value" --output tsv)
 
 $containerGroupName = "transcode"
+
+$commandLine = "ffmpeg -i /mnt/azfile/$filename -vf" + `
+    " ""thumbnail,scale=640:360"" -frames:v 1 /mnt/azfile/thumb.png"
+
 az container create `
     -g $resourceGroup `
     -n $containerGroupName `
@@ -36,7 +47,7 @@ az container create `
     --azure-file-volume-account-key $storageKey `
     --azure-file-volume-share-name $shareName `
     --azure-file-volume-mount-path "/mnt/azfile" `
-    --command-line "ffmpeg -i /mnt/azfile/$filename -vf  ""thumbnail,scale=640:360"" -frames:v 1 /mnt/azfile/thumb.png"
+    --command-line $commandLine
 
 az container logs -g $resourceGroup -n $containerGroupName 
 
@@ -45,7 +56,8 @@ az container show -g $resourceGroup -n $containerGroupName --query provisioningS
 az storage file list -s $shareName -o table
 
 $downloadThumbnailPath = "C:\Users\markh\Downloads\thumb.png"
-az storage file download -s $shareName -p "thumb.png" --dest $downloadThumbnailPath
+az storage file download -s $shareName -p "thumb.png" `
+    --dest $downloadThumbnailPath
 Start-Process $downloadThumbnailPath
 
 #az container delete -g $resourceGroup -n $containerGroupName
